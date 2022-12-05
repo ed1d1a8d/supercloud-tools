a26f04dd0d8afb648244f856c64787524a917840
Tony Wang <twang6@mit.edu>
2022-12-05 11:01:37 -0500

    Change jupyter log location

--- scripts/req-jup.sh
+++ scripts/req-jup.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-sbatch --open-mode=append --qos high -J Jup_$1 --partition=xeon-g6-volta --constraint=xeon-g6 --gres=gpu:volta:1 --cpus-per-task=20 --time='24:00:00' --wrap 'srun --resv-ports=1 /usr/local/bin/LL_start_jupyter_notebook.sh --notebook -- anaconda/2022a' 2>&1
+mkdir -p $HOME/slurm-logs/jupyter
+sbatch -J Jup_$1 \
+    --open-mode=append \
+    --qos high \
+    --partition=xeon-g6-volta \
+    --constraint=xeon-g6 \
+    --gres=gpu:volta:1 \
+    --cpus-per-task=20 \
+    --time='24:00:00' \
+    -o $HOME/slurm-logs/jupyter/slurm-%j.out \
+    --wrap 'srun --resv-ports=1 /usr/local/bin/LL_start_jupyter_notebook.sh --notebook -- anaconda/2022a' \
+    2>&1
