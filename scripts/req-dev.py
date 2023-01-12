1529f26b6e49c5b063750d58a02f12137b65544e
Tony Wang <twang6@mit.edu>
2023-01-11 19:48:44 -0500

    Make better req-dev script in python

---
+++ scripts/req-dev.py
@@ -0,0 +1,80 @@
+#!/state/partition1/llgrid/pkg/anaconda/anaconda3-2021b/bin/python
+import argparse
+import dataclasses
+import pathlib
+import subprocess
+
+
+@dataclasses.dataclass
+class ProgramArgs:
+    job_name: str = dataclasses.field(
+        default="devbox", metadata={"help": "Name of the job."}
+    )
+
+    cpus: int = dataclasses.field(
+        default=20,
+        metadata={
+            "help": " ".join(
+                [
+                    "Number of cpus to request.",
+                    "4GB of RAM is allocated per cpu.",
+                ]
+            )
+        },
+    )
+
+    gpus: int = dataclasses.field(
+        default=2, metadata={"help": "Number of gpus to request."}
+    )
+
+    hours: int = dataclasses.field(
+        default=24, metadata={"help": "Number of hours to request."}
+    )
+
+    @classmethod
+    def from_cli(cls):
+        parser = argparse.ArgumentParser(description="Launches a devbox.")
+
+        for field in dataclasses.fields(cls):
+            parser.add_argument(
+                f"--{field.name.replace('_', '-')}",
+                default=field.default,
+                type=field.type,
+                help=field.metadata["help"],
+            )
+
+        return cls(**vars(parser.parse_args()))
+
+
+def main(args: ProgramArgs):
+    # Get absolute path to mallory
+    script_dir = pathlib.Path(__file__).parent.resolve()
+    mallory_path = (script_dir.parent / "bin" / "mallory").absolute()
+
+    # Make log directory
+    # mkdir -p $HOME/slurm-logs/dev
+    log_dir = (pathlib.Path.home() / "slurm-logs" / "dev").absolute()
+    log_dir.mkdir(parents=True, exist_ok=True)
+
+    # Construct sbatch command
+    sbatch_args = [
+        "sbatch",
+        "--qos=high",
+        f"--job-name={args.job_name}",
+        f"--cpus-per-task={args.cpus}",
+        f"--time={args.hours}:00:00",
+        f"--output={log_dir}/{args.job_name}-%j.out",
+        "--wrap",
+        f"srun {mallory_path}",
+    ] + (
+        [f"--gres=gpu:volta:{args.gpus}"]
+        if args.gpus > 0
+        else ["--partition=xeon-p8"]  # Use p8 partition for cpu-only jobs
+    )
+
+    # Launch job with sbatch
+    subprocess.run(sbatch_args, check=True)
+
+
+if __name__ == "__main__":
+    main(ProgramArgs.from_cli())
